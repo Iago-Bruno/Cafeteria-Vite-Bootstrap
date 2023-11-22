03f41d89f4e276d006ca3965c65547f9a4796deb
iagobruno <iagobru1258@gmail.com>
2023-11-22 15:00:05 -0300

    Adicionando um campo de filtragem

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Outlet } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Outlet } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.css';
 
 function App() {
   return (
     <div id="App">
-      <Header />
       <Container className="App_content">
+        <Header />
         <Outlet />
       </Container>
       <Footer />
--- src/pages/cardapio/Cardapio.jsx
+++ src/pages/cardapio/Cardapio.jsx
@@ -1,65 +1,88 @@
-import { Table } from 'react-bootstrap';
+import { Button, Form, Table } from 'react-bootstrap';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import UsersData from '../../dataSet/UsersData';
+import { useState } from 'react';
 
 const Cardapio = () => {
+  const [filter, setFilter] = useState();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(filter);
+  };
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Nome</th>
-          <th>Usuário</th>
-          <th>Email</th>
-          <th>Endereço</th>
-          <th>Localização</th>
-        </tr>
-      </thead>
-      <tbody>
-        {UsersData.map((user, index) => {
-          return (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{user.name}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>
-                {user.address.street}, {user.address.city}, {user.address.suite}
-                , {user.address.zipcode}
-              </td>
-              <td style={{ maxHeight: '400px' }}>
-                <MapContainer
-                  center={[user.address.geo.lat, user.address.geo.lng]}
-                  zoom={13}
-                  style={{
-                    width: '300px',
-                    height: '200px',
-                  }}
-                >
-                  <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  />
-                  <Marker
-                    position={[user.address.geo.lat, user.address.geo.lng]}
+    <>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="formFilter">
+          <Form.Label>Buscar</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={(event) => setFilter(event.target.value)}
+            placeholder="Digite um alimento"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Buscar
+        </Button>
+      </Form>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Usuário</th>
+            <th>Email</th>
+            <th>Endereço</th>
+            <th>Localização</th>
+          </tr>
+        </thead>
+        <tbody>
+          {UsersData.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.address.street}, {user.address.city},{' '}
+                  {user.address.suite}, {user.address.zipcode}
+                </td>
+                <td style={{ maxHeight: '400px' }}>
+                  <MapContainer
+                    center={[user.address.geo.lat, user.address.geo.lng]}
+                    zoom={13}
+                    style={{
+                      width: '300px',
+                      height: '200px',
+                    }}
                   >
-                    <Popup maxWidth={200}>
-                      <h4 style={{ fontSize: '16px', fontWeight: 'bold' }}>
-                        {user.company.name}
-                      </h4>
-                      <h6 style={{ fontSize: '12px' }}>
-                        {user.company.catchPhrase} <br /> {user.company.bs}
-                      </h6>
-                    </Popup>
-                  </Marker>
-                </MapContainer>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+                    <TileLayer
+                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker
+                      position={[user.address.geo.lat, user.address.geo.lng]}
+                    >
+                      <Popup maxWidth={200}>
+                        <h4 style={{ fontSize: '16px', fontWeight: 'bold' }}>
+                          {user.company.name}
+                        </h4>
+                        <h6 style={{ fontSize: '12px' }}>
+                          {user.company.catchPhrase} <br /> {user.company.bs}
+                        </h6>
+                      </Popup>
+                    </Marker>
+                  </MapContainer>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
